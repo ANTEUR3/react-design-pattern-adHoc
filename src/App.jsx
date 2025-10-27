c718440db9c694214bfdb2214b294f64321ec671
ANTEUR3 <anteuryounes1@gmail.com>
2025-10-27 06:55:01 +0100

    project initialisation

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import react,{ useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from './components/ui/button'
+import { buttonVariants } from './components/ui/button'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
